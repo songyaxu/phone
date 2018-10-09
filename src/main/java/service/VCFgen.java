27b3122f9217ce26b9ad9f5f97e853abca364995
songyaxu <syx1065001748@126.com>
2018-10-09 15:06:13 +0800

    添加文件读取内容完善

--- src/main/java/service/VCFgen.java
+++ src/main/java/service/VCFgen.java
@@ -2,8 +2,12 @@ package service;
 
 import bean.PhoneResult;
 import client.VCFGenerator;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.stream.JsonReader;
+import lombok.extern.slf4j.Slf4j;
 
-import java.io.File;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,15 +19,34 @@ import java.util.List;
  */
 public class VCFgen {
 
+    private String test = "PhoneResult(mts=1810046, province=内蒙古, catName=中国电信, telString=18100461272, areaVid=30495, ispVid=138238560, carrier=内蒙古电信)";
 
-    public List<PhoneResult> getPhoneResultList(String fileLocation){
+    private List<PhoneResult> getPhoneResultList(String fileLocation){
         List<PhoneResult> resultList= new ArrayList<PhoneResult>();
-        File file = new File(fileLocation);
-        while ()
+        try {
+            FileInputStream fileInputStream = new FileInputStream(fileLocation);
+            JsonReader jsonReader = new JsonReader(new InputStreamReader(fileInputStream,"UTF-8"));
+            Gson gson = new GsonBuilder().create();
+            jsonReader.beginArray();
+            while (jsonReader.hasNext()) {
+                PhoneResult result = gson.fromJson(jsonReader, PhoneResult.class);
+                resultList.add(result);
+                System.out.println("读取成功 ：" + result);
+            }
+            jsonReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("找不到指定文件：" + fileLocation);
+        } catch (UnsupportedEncodingException e) {
+            System.out.println("不支持的编码格式");
+        } catch (IOException e){
+            System.out.println("输入流异常");
+        }
+        return resultList;
     }
 
     public static void main(String[] args) {
-        VCFGenerator client = new VCFGenerator();
-        System.out.println(client.genVCFString("白","白","15326085176","测试"));
+        VCFgen vcFgen = new VCFgen();
+        List<PhoneResult> resultList = vcFgen.getPhoneResultList("C:\\Users\\yaxuSong\\Desktop\\vcf.json");
+        System.out.println(resultList.size());
     }
 }
